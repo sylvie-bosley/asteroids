229c14b909fa5bc6b87dd42de598cfea4e6dd8c1
pierce-bosley <pierce.bosley@gmail.com>
2021-07-05 18:52:58 -0700

    Reorganize project and implement basic functionality
    Final decision on how to organize the prodject folder. Will not be
    including the SFML files but will be documenting current dependencies
    in the readme file. Also implimented basic window, graphics, and audio
    functionality in main.cpp as a starting point. Consider this the
    starting point of the project.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-"# asteroids" 
+"# asteroids"
 
 basic asteroids game implemented in c++
+
+dependencies:
+mingw-w64 (gcc) version 7.3.0
+  install options: i686-7.3.0-posix-dwarf-rt_v5-rev0
+SFML version 2.5.1 - 32-bit
--- code/main.cpp
+++ code/main.cpp
@@ -1,13 +1,58 @@
-#include "asteroids.h"
-#include "helpers.h"
+#include "include/asteroids.h"
+#include "include/helpers.h"
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main(int argc, char *argv[]) {
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  sf::Font font;
+  if (!font.loadFromFile("data/test/sansation.ttf"))
+    return 1;
+  sf::Text text;
+  text.setFont(font);
+
   if (argc > 1)
-    std::cout << "Hello " << Asteroids::capitalize(argv[1]) << "!\n";
+    text.setString("Hello " + Asteroids::capitalize(argv[1]) + "!");
   else
-    std::cout << "Hello user!\n";
+    text.setString("Hello user!");
+
+  text.setCharacterSize(24);
+  text.setFillColor(sf::Color::Red);
+  text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+  float x_pos = 100.f - (text.getGlobalBounds().width / 2.f);
+  float y_pos = 100.f - (text.getGlobalBounds().height);
+  text.setPosition(x_pos, y_pos);
+
+  sf::SoundBuffer buffer;
+  sf::Sound sound;
+  if (!buffer.loadFromFile("data/test/ball.wav"))
+    return 1;
+  sound.setBuffer(buffer);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.draw(text);
+    window.display();
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) &&
+        sound.getStatus() == sf::Sound::Stopped)
+      sound.play();
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+      break;
+  }
 
   return 0;
 }
